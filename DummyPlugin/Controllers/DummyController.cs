e7e6a1fd2e278ff90ac1f16afbaf928c87f2f71d
Christopher R. Schuchardt <Chris.Schuchardt@pcworldinc.com>
2023-08-29 10:32:08 -0400

    added comments to the DummyController to help users understand whats going on.

--- DummyPlugin/Controllers/DummyController.cs
+++ DummyPlugin/Controllers/DummyController.cs
@@ -8,10 +8,10 @@ using System.Net.Mime;
 
 namespace Neo.Plugins.DummyPlugin.Controllers
 {
-    [Route("/api/v1")]
-    [Produces(MediaTypeNames.Application.Json)]
-    [Consumes(MediaTypeNames.Application.Json)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(EmptyErrorModel))]
+    [Route("/api/v1")] // <-- Route/Directory URL (cna be new or add to existing)
+    [Produces(MediaTypeNames.Application.Json)] // <-- must have set to application/json. if you for get its not the end of the world.
+    [Consumes(MediaTypeNames.Application.Json)] // <-- must have set to application/json. if you for get its not the end of the world.
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(EmptyErrorModel))] // <-- Must put this on each controller you have. So Swagger knows the default error type model.
     [ApiExplorerSettings(GroupName = "v1")]
     [ApiController]
     public class DummyController : ControllerBase
@@ -24,31 +24,31 @@ namespace Neo.Plugins.DummyPlugin.Controllers
         }
 
         /// <summary>
-        /// Say Hello
+        /// Say Hello <!-- This is what the use see's in Swagger or Open API Clients-->
         /// </summary>
         /// <param name="scripthash" example="0xed7cc6f5f2dd842d384f254bc0c2d58fb69a4761">ScriptHash</param>
         /// <returns>string</returns>
         /// <response code="200">Successful</response>
-        /// <response code="400">If anything is invalid or request crashes.</response>
-        [HttpGet("contract/{hash:required}/sayHello", Name = "GetSayHello")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
-        public IActionResult GetSayHello(
+        /// <response code="400">An error occurred. See Response for details.</response> <!--This line is key to what the users see's as an error message dont change it-->
+        [HttpGet("contract/{hash:required}/sayHello", Name = "GetSayHello")] // <-- Name is the name of the method for Swagger or Open API client to generate. Note that path parameters are the same name for Swagger methods.
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))] // <-- Must put this on each controller method you have. So Swagger knows the default return type model.
+        public IActionResult GetSayHello( // <-- You dont have to use IActionResult you can just return the type you would like. I like doing it this way so I am able to throw other responses.
             [FromRoute(Name = "hash")]
             UInt160 scripthash)
         {
-            return Ok($"Hello, {scripthash}");
+            return Ok($"Hello, {scripthash}"); 
         }
 
         /// <summary>
-        /// Get block by block hash
+        /// Get block by block hash <!-- This is what the use see's in Swagger or Open API Clients-->
         /// </summary>
         /// <param name="blockhash" example="0xad83d993ca2d9783ca86a000b39920c20508c8ccae7b7db11806646a4832bc50">Hash256</param>
         /// <returns>Block Object</returns>
         /// <response code="200">Successful</response>
-        /// <response code="400">If anything is invalid or request crashes.</response>
-        [HttpGet("ledger/block/{hash:required}", Name = "GetBlockByHash")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Block))]
-        public IActionResult GetBlockByHash(
+        /// <response code="400">An error occurred. See Response for details.</response> <!--This line is key to what the users see's as an error message dont change it-->
+        [HttpGet("ledger/block/{hash:required}", Name = "GetBlockByHash")] // <-- Name is the name of the method for Swagger or Open API client to generate. Note that path parameters are the same name for Swagger methods.
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Block))] // <-- Must put this on each controller method you have. So Swagger knows the default return type model.
+        public IActionResult GetBlockByHash( // <-- You dont have to use IActionResult you can just return the type you would like. I like doing it this way so I am able to throw other responses.
             [FromRoute(Name = "hash")]
             UInt256 blockhash)
         {
